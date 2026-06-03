1688eb516d9fa5886075fbd47c7fd84af0f05057
felpsgames-cityboy <felipe.figueiroa@escola.pr.gov.br>
2026-06-03 13:53:18 +0000

    v.1

--- animal.js
+++ animal.js
@@ -1,1 +1,10 @@
+const animal = {
+    nome: "Felps", //string
+    especie: "Gato",
+    idade: 7, //number
+    tutor: false,
+    vacinado: true
+};
 
+//console.log(animal);
+console.log(animal.nome);
